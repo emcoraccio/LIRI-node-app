6b8f4daeecf94dad0bda252a577f91a204792426
Em Coraccio <emcoraccio@gmail.com>
2019-08-31 10:43:41 -0400

    log movie data complete

--- search.js
+++ search.js
@@ -13,31 +13,6 @@ let searchTerm = process.argv.slice(3).join("+")
 
 // LOGGING DATA FUNCTIONS - TO BE CALLED IN SEARCH FUNCTIONS
 
-let logSongData = (data) => {
-
-  // set variables that access accurate info in response
-  let artists = data.tracks.items[0].artists[0].name
-  let songName = data.tracks.items[0].name;
-  let albumName = data.tracks.items[0].album.name;
-  let songPreview = data.tracks.items[0].preview_url;
-  let songHref = data.tracks.items[0].href;
-  let preview;
-
-  //  conditional to display correct link for preview
-  songPreview ? preview = `preview the song: ${songPreview}` :
-      songHref ? preview = `No preview available. View full song: ${songHref}` :
-      preview = `No preview available`
-
-  console.log(`
-  artists: ${artists}
-  song-title: ${songName}
-  album-title: ${albumName}
-  ${preview}
-  `)
-
-}
-
-
 let logConcertData = (response) => {
 
   let artist = process.argv.slice(3).join(" ")
@@ -55,6 +30,60 @@ let logConcertData = (response) => {
   `)
 
 }
+
+
+
+let logSongData = (data) => {
+
+  // set variables that access accurate info in response
+  let artists = data.tracks.items[0].artists[0].name;
+  let songName = data.tracks.items[0].name;
+  let albumName = data.tracks.items[0].album.name;
+  let songPreview = data.tracks.items[0].preview_url;
+  let songHref = data.tracks.items[0].href;
+  let preview;
+
+  //  conditional to display correct link for preview
+  songPreview ? preview = `preview the song: ${songPreview}` :
+    songHref ? preview = `No preview available. View full song: ${songHref}` :
+      preview = `No preview available`
+
+  console.log(`
+  artists: ${artists}
+  song-title: ${songName}
+  album-title: ${albumName}
+  ${preview}
+  `)
+
+}
+
+
+let logMovieData = (response) => {
+
+  console.log(response.data)
+
+  let movieTitle = response.data.Title;
+  let year = response.data.Year;
+  let imdbRating = response.data.Ratings[0].Value;
+  let rottenTomatoRating = response.data.Ratings[1].Value;
+  let countryProduced = response.data.Country;
+  let language = response.data.Language;
+  let plot = response.data.Plot;
+  let actors = response.data.Actors;
+
+  console.log(`
+  Title: ${movieTitle}
+  Year: ${year}
+  IMDB Rating: ${imdbRating}
+  Rotten Tomatoes Rating: ${rottenTomatoRating}
+  Country Produced: ${countryProduced}
+  Language: ${language}
+  Actors: ${actors}
+  Plot: ${plot}
+  `)
+
+}
+
 
 
 // SEARCH FUNCTIONS - GET DATA FROM APIS
@@ -76,24 +105,30 @@ let searchConcert = () => {
 
 let searchSong = () => {
 
-  spotify.search({ type: 'track', query: `${searchTerm}` }, function (err, data) {
-    
-    if (err) {
-      return console.log('Error occurred: ' + err);
-    }
+  !searchTerm ? searchTerm = "The+Sign" : searchTerm = searchTerm
 
-    logSongData(data);
+    spotify.search({ type: 'track', query: `${searchTerm}` }, function (err, data) {
 
-  });
+      if (err) {
+        return console.log('Error occurred: ' + err);
+      }
+
+      logSongData(data);
+
+    });
 
 }
 
 
 let searchMovie = () => {
 
-  let queryURL = ``;
+  !searchTerm ? searchTerm = "Mr.+Nobody" : searchTerm = searchTerm
+
+  let queryURL = `http://www.omdbapi.com/?apikey=c5d29ec1&t=${searchTerm}`;
 
   axios.get(queryURL).then((response) => {
+
+    logMovieData(response)
 
   })
 
